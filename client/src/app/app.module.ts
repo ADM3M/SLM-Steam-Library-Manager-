40f28ae8d10d5aef03a045114f0aa3a13f52eccd
ADM3M <fly.swift499@gmail.com>
2022-06-07 22:51:45 +0300

    Encapsulated nav bar and body container

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './home/home.component';
+import { HomeCardComponent } from './home/home-card/home-card.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    HomeCardComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
