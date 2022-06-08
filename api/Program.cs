c687a14a8139ae52ef26e86d32ca98dcfe84922a
ADM3M <fly.swift499@gmail.com>
2022-06-08 23:30:20 +0300

    Created and injected Automapper profile.

--- api/Program.cs
+++ api/Program.cs
@@ -1,4 +1,5 @@
 using api.Data;
+using api.Helpers;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,6 +10,7 @@ builder.Services.AddDbContext<DataContext>(options =>
 {
     options.UseSqlite(builder.Configuration.GetConnectionString("default"));
 });
+builder.Services.AddAutoMapper(typeof(AutomapperProfile).Assembly);
 
 var app = builder.Build();
 
