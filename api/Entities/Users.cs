bb0d6d046c5404dceb79599bdf0126ba5dd162d3
ADM3M <fly.swift499@gmail.com>
2022-06-10 15:01:08 +0300

    Added SteamId, Password propertyes to Users entity

--- api/Entities/Users.cs
+++ api/Entities/Users.cs
@@ -4,7 +4,11 @@ public class Users
 {
     public int Id { get; set; }
 
+    public long SteamId { get; set; }
+    
     public string UserName { get; set; }
+
+    public string Password { get; set; }
 
     public string PhotoUrl { get; set; }
 
