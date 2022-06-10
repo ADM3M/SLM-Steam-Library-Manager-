6f921963a391190623ccf62abaae814181b6ad62
ADM3M <fly.swift499@gmail.com>
2022-06-10 14:10:24 +0300

    Configured database with fluent API

--- api/Data/DataContext.cs
+++ api/Data/DataContext.cs
@@ -9,12 +9,28 @@ public class DataContext : DbContext
     
     public DataContext(DbContextOptions options) : base(options) {}
 
-    public DbSet<User> User { get; set; }
+    public DbSet<Users> User { get; set; }
+
+    public DbSet<Games> Games { get; set; }
+
+    public DbSet<UserGames> UserGames { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
 
-        
+        modelBuilder.Entity<UserGames>().HasKey(key => new {key.GameId, key.UserId});
+
+        modelBuilder.Entity<UserGames>()
+            .HasOne(ug => ug.User)
+            .WithMany(u => u.Colection)
+            .HasForeignKey(f => f.UserId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        modelBuilder.Entity<UserGames>()
+            .HasOne(ug => ug.Game)
+            .WithMany(g => g.collection)
+            .HasForeignKey(f => f.GameId)
+            .OnDelete(DeleteBehavior.Cascade);
     }
 }
