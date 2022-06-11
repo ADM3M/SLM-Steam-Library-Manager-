064c50cdaa8ebefdaddc725ea677f0ea53915551
ADM3M <fly.swift499@gmail.com>
2022-06-11 16:16:32 +0300

    Rebased login modal logic to service. Added baseUrl to environment.

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
-import { BsModalRef, BsModalService, ModalOptions } from 'ngx-bootstrap/modal';
-import { RegisterLoginModalComponent } from './modals/register-login-modal/register-login-modal.component';
+import { BsModalService } from 'ngx-bootstrap/modal';
+import { IUser } from './models/user';
 import { AccountService } from './services/account.service';
 
 @Component({
@@ -11,26 +11,19 @@ import { AccountService } from './services/account.service';
 })
 export class AppComponent implements OnInit {
   title = 'client';
-  private bsModalRef?: BsModalRef;
 
   constructor(private modalService: BsModalService, public accService: AccountService) {}
   
   ngOnInit(): void {
-    if (!this.accService.userLogged) {
-      this.showModal();
+    const userJson = localStorage.getItem('user');  
+    
+    if (userJson) {
+      const user: IUser = JSON.parse(userJson);
+      this.accService.setCurrentUser(user);
+      return;
     }
+    
+    this.accService.showLoginModal();
   }
-  
-  showModal() {
-    const config: ModalOptions = {
-      initialState: {
-        title: 'Login',
-      },
 
-      ignoreBackdropClick: true,
-    };
-
-    this.bsModalRef = this.modalService.show(RegisterLoginModalComponent, config);
-    this.bsModalRef.content.closeBtnName = 'Close';
-  }
 }
--- client/src/app/services/account.service.ts
+++ client/src/app/services/account.service.ts
@@ -1,7 +1,10 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { BsModalRef, BsModalService, ModalOptions } from 'ngx-bootstrap/modal';
 import { Observable, ReplaySubject } from 'rxjs';
 import { map, take } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
+import { RegisterLoginModalComponent } from '../modals/register-login-modal/register-login-modal.component';
 import { IUser } from '../models/user';
 
 @Injectable({
@@ -9,15 +12,16 @@ import { IUser } from '../models/user';
 })
 export class AccountService {
 
-  private baseUrl?: string;
+  private baseUrl? = environment.baseUrl;
   private currentUserSource = new ReplaySubject<IUser>(1);
   public currentUser$ = this.currentUserSource.asObservable();
-  public userLogged = false;
+  
+  private registerModal?: BsModalRef;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private modalService: BsModalService) { }
 
   public login(model: any): Observable<void> {
-    return this.http.post<IUser>(this.baseUrl + "account/login", model).pipe(
+    return this.http.post<IUser>(this.baseUrl + "Account/login", model).pipe(
       map(user => {
         if (user) {
           this.setCurrentUser(user);
@@ -37,18 +41,30 @@ export class AccountService {
   }
 
   public setCurrentUser(user: IUser): void {
+    console.log(user);
     localStorage.setItem('user', JSON.stringify(user));
     this.currentUserSource.next(user);
-    this.userLogged = true;
+    this.registerModal?.hide();
   }
 
   public logout(): void {
     this.currentUserSource.next(undefined);
     localStorage.removeItem('user');
-    this.userLogged = false;
   }
 
   getDecodedToken(token: any) {
     return JSON.parse(atob(token.split(".")[1]))
+  }
+
+  showLoginModal() {
+    const config: ModalOptions = {
+      initialState: {
+        title: 'Login',
+      },
+
+      ignoreBackdropClick: true,
+    };
+
+    this.registerModal = this.modalService.show(RegisterLoginModalComponent, config);
   }
 }
