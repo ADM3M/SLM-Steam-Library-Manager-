015ad403067c7269d2dd0d89018516d85ab460ec
ADM3M <fly.swift499@gmail.com>
2022-06-15 21:20:59 +0300

    Fixed algorythm. Updated change detection strategy

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,3 +1,4 @@
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { Component, OnInit } from '@angular/core';
 import { pipe } from 'rxjs';
 import { take } from 'rxjs/operators';
