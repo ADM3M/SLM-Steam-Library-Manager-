e6acf2d9c08799ee144bb4e11a27605053b4c779
ADM3M <fly.swift499@gmail.com>
2022-06-16 22:34:52 +0300

    Added ngx-spinner. Created busyService. Created loading.interceptor styled ngx-spinner.

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { ModalModule } from 'ngx-bootstrap/modal';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
+import { NgxSpinnerModule } from "ngx-spinner"
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -13,6 +14,7 @@ import { HomeCardComponent } from './home/home-card/home-card.component';
 import { NavComponent } from './nav/nav.component';
 import { RegisterLoginModalComponent } from './modals/register-login-modal/register-login-modal.component';
 import { JwtInterceptor } from './interceptors/jwt.interceptor';
+import { LoadingInterceptor } from './interceptors/loading.interceptor';
 
 @NgModule({
   declarations: [
@@ -30,9 +32,11 @@ import { JwtInterceptor } from './interceptors/jwt.interceptor';
     CommonModule,
     ModalModule.forRoot(),
     ReactiveFormsModule,
+    NgxSpinnerModule
   ],
   providers: [
-    {provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true}
+    {provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true},
+    {provide: HTTP_INTERCEPTORS, useClass: LoadingInterceptor, multi: true},
   ],
   bootstrap: [AppComponent]
 })
