f62d759e0fab0cde01afcaecd4fd3c4ba2200473
ADM3M <fly.swift499@gmail.com>
2022-06-16 22:35:52 +0300

    New games now join list from the start

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -57,7 +57,7 @@ export class HomeComponent implements OnInit {
         }
 
         this.memberService.addGames(gamesToAdd).pipe(take(1)).subscribe((newGames) => {
-          this.memberService.userGamesSource.next(dbGames.concat(newGames));
+          this.memberService.userGamesSource.next(newGames.concat(dbGames));
         });
       })
     })
