f07eb89d80e4022ccba29a174b08d2c71f75cc68
ADM3M <fly.swift499@gmail.com>
2022-06-19 03:21:13 +0300

    Implemented filtering and sorting. Cleaned up a component.

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,7 +1,5 @@
-import { ChangeDetectionStrategy, ChangeDetectorRef, Component, Input, OnInit, TemplateRef } from '@angular/core';
-import { forkJoin, Observable, of, Subject, zip } from 'rxjs';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { delay, map, take } from 'rxjs/operators';
-import { DisplayParams } from '../models/displayParams';
 import { SortObj } from '../models/sortObj';
 import { AccountService } from '../services/account.service';
 import { MemberService } from '../services/member.service';
@@ -15,15 +13,20 @@ import { SteamService } from '../services/steam.service';
 })
 export class NavComponent implements OnInit {
   public isFetchNeeded = false;
-  public sort = this.memberService.sortBy;
-  public displayParams = new DisplayParams();
+  public sort = this.memberService.displayModel.sortBy;
+  public gamesName: string[] = [];
 
   constructor(public readonly accService: AccountService,
     private readonly steamService: SteamService,
     public readonly memberService: MemberService,
     private readonly changeDetectorRef: ChangeDetectorRef) { }
 
-  turnSort(id: number): void {
+  ngOnInit(): void {
+    this.getGamesName();
+    this.IsFetchAvailiable();
+  }
+
+  public turnSort(id: number): void {
     this.sort.forEach((el, i) => {
       if (i !== id) {
         el.reset();
@@ -32,14 +35,27 @@ export class NavComponent implements OnInit {
         el.reverseVal();
       }
     })
+
+    this.fetchDbGamesWithParams(1);
   }
 
-  getSortValue(id: number): SortObj {
+  public resetInput(): void {
+    if (this.memberService.displayParams.search === "") {
+      this.memberService.displayModel.search = "";
+      return;
+    }
+
+    this.memberService.displayParams.search = '';
+    this.memberService.displayModel.search = '';
+    this.fetchDbGamesWithParams(1);
+  }
+
+  public getSortValue(id: number): SortObj {
     return this.sort[id];
   }
 
   public turnFilter(name: string): void {
-    const filters = this.memberService.filters;
+    const filters = this.memberService.displayModel.filters;
 
     switch (name) {
       case "notSet":
@@ -53,23 +69,46 @@ export class NavComponent implements OnInit {
       case "completed":
         filters.completed = !filters.completed;
         break;
+
+      case "backlog":
+        filters.backlog = !filters.backlog;
+        break;
     }
+
+    this.fetchDbGamesWithParams(1);
   }
 
   public getFilterVal(name: string): boolean {
+    const filters = this.memberService.displayModel.filters;
+
     if (name === "notSet") {
-      return this.memberService.filters.notSet;
+      return filters.notSet;
     }
 
     if (name === "inProgress") {
-      return this.memberService.filters.inProgress;
+      return filters.inProgress;
     }
 
-    return this.memberService.filters.completed;
+    if (name === "completed") {
+      return filters.completed;
+    }
+
+    return filters.backlog;
+  }
+
+  public getGamesName(): void {
+    this.accService.currentUser$.pipe(take(1), map(u => u.steamId)).subscribe(steamId => {
+      if (!steamId) {
+        return;
+      }
+
+      this.memberService.getGamesName().pipe(take(1), map(names => {
+        this.gamesName = names;
+      })).subscribe();
+    });
   }
 
   public fetchSteamGames(steamId: string): void {
-
     if (!this.isFetchNeeded) {
       return;
     }
@@ -82,21 +121,23 @@ export class NavComponent implements OnInit {
       })
   }
 
-  public getDbGamesName(): Observable<string[]> {
-    return this.memberService.getGamesName()
+  public fetchDbGamesWithParams(pageNumber: number): void {
+    this.memberService.setDisplayParams();
+    this.memberService.getPaginatedUserGames(pageNumber).pipe(take(1)).subscribe(games => {
+      this.memberService.userGamesSource.next(games);
+    });
   }
 
   public IsFetchAvailiable(): void {
     this.accService.currentUser$.pipe(take(1), map(user => user.steamId))
       .subscribe(steamId => {
-        forkJoin([this.getDbGamesName(), this.steamService.getGamesCount(steamId)]).pipe(
-          delay(1500),
-          map(([dbGameNames, steamGamesCount]) => {
-            console.log(`dbgames: ${dbGameNames.length} | steamGames: ${steamGamesCount}`);
-            this.displayFetchButton(dbGameNames.length < steamGamesCount);
+        this.steamService.getGamesCount(steamId).pipe(delay(1500),
+          map((steamGamesCount) => {
+            console.log(`dbgames: ${this.gamesName.length} | steamGames: ${steamGamesCount}`);
+            this.displayFetchButton(this.gamesName.length < steamGamesCount);
           }),
           take(1)).subscribe();
-      })
+      });
   }
 
   public displayFetchButton(value: boolean): void {
@@ -104,13 +145,10 @@ export class NavComponent implements OnInit {
     this.changeDetectorRef.detectChanges();
   }
 
-  public logOut() {
+  public logOut(): void {
     this.accService.logout();
+    this.memberService.userGamesSource.next(undefined);
+    this.memberService.pagination.totalItems = 0;
+    console.log(this.memberService.pagination);
   }
-
-  ngOnInit(): void {
-    this.IsFetchAvailiable();
-    this.memberService.getGamesName().pipe(take(1)).subscribe();
-  }
-
 }
