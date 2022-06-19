975fdbda85954de244c7eb1b141007e56a689081
ADM3M <fly.swift499@gmail.com>
2022-06-19 03:19:09 +0300

    Changed config.ts Implemented dynamic games loading. Styled a template. Cleaned up a component.

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,7 +1,6 @@
 import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { forkJoin } from 'rxjs';
 import { map, take } from 'rxjs/operators';
-import { IGameObj } from '../models/gameObj';
 import { ISteamGame } from '../models/steamGame';
 import { AccountService } from '../services/account.service';
 import { MemberService } from '../services/member.service';
@@ -14,31 +13,51 @@ import { SteamService } from '../services/steam.service';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class HomeComponent implements OnInit {
-  
-  constructor(public readonly accService: AccountService, 
-    public readonly steamService: SteamService, 
+
+  constructor(public readonly accService: AccountService,
+    public readonly steamService: SteamService,
     public readonly memberService: MemberService,
     private readonly changeDetectorRef: ChangeDetectorRef) { }
 
-  private getGamesFromBd(): void {
-    this.memberService.getUserDbGames(1).pipe(take(1)).subscribe(games => {
-      this.memberService.userGamesSource.next(games);
-      
-      this.changeDetectorRef.markForCheck();
-    });
+  ngOnInit(): void {
+    this.onUserLogin();
+  }
+
+  public getGamesFromBd(): void {
+    const pag = this.memberService.pagination;
+
+    if (pag.currentPage >= pag.totalPages) {
+      return;
+    }
+
+    forkJoin([this.memberService.getPaginatedUserGames(pag.currentPage + 1)
+      .pipe(take(1)), this.memberService.games$.pipe(take(1))]).pipe(
+        take(1),
+        map(([newGames, currentGames]) => {
+          newGames.forEach(game => {
+            currentGames.push(game);
+          });
+          this.memberService.userGamesSource.next(currentGames);
+        })
+      ).subscribe();
+
+    this.changeDetectorRef.markForCheck()
   }
 
   private onUserLogin(): void {
     this.accService.currentUser$.pipe(take(1)).subscribe(() => {
-      this.getGamesFromBd();
+      this.memberService.getPaginatedUserGames(1).pipe(take(1)).subscribe(games => {
+        this.memberService.userGamesSource.next(games);
+      })
+
       this.fetchSteamGames();
     });
   }
 
   private fetchSteamGames(): void {
-    forkJoin([this.memberService.games.pipe(take(1)), this.memberService.getUserDbGames(-1)])
+    forkJoin([this.memberService.games$.pipe(take(1)), this.memberService.getUserDbGames()])
       .pipe(map(([pagedGames, dbGames]) => {
-        this.steamService.steamGames.pipe(take(1)).subscribe((steamGames: ISteamGame[]) => {
+        this.steamService.steamGames$.pipe(take(1)).subscribe((steamGames: ISteamGame[]) => {
           let gamesToAdd: ISteamGame[] = [];
 
           if (dbGames.length === 0) {
@@ -72,9 +91,22 @@ export class HomeComponent implements OnInit {
       })).pipe(take(1)).subscribe();
   }
 
-  ngOnInit(): void {
-    this.onUserLogin();
+  public loadButtonDisplay(): boolean {
+    const pag = this.memberService.pagination;
+    return pag.currentPage < pag.totalPages
+      && pag.totalItems > 0;
   }
 
-}
+  // infinite scroll
 
+  // trackScroll() {
+  //   fromEvent(window, "scroll").subscribe(() => {
+  //     if (document.documentElement.scrollHeight - window.scrollY - window.innerHeight < 0.0003 ) {
+  //       console.log(this.memberService.pagination);
+  //       if (this.memberService.pagination.currentPage < this.memberService.pagination.totalPages) {
+  //         this.getGamesFromBd(this.memberService.pagination.currentPage + 1);
+  //       }
+  //     }
+  //   })
+  // }
+}
