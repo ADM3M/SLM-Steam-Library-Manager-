7b3703550a5767cc4754eafc0aa62f996f273e39
ADM3M <fly.swift499@gmail.com>
2022-07-01 16:28:09 +0300

    Updated message service for signalR hubs.

--- client/src/app/services/message.service.ts
+++ client/src/app/services/message.service.ts
@@ -1,16 +1,51 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { HubConnection, HubConnectionBuilder } from '@microsoft/signalr';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { take } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 import { IMessage } from '../models/message';
+import { IUser } from '../models/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MessageService {
   private baseUrl = environment.baseUrl + "message/";
+  private hubUrl = environment.baseHub;
+  private hubConnection: HubConnection;
+  private messageThreadSource = new BehaviorSubject<IMessage[]>([]);
+  messageThread$ = this.messageThreadSource.asObservable();
 
   constructor(private readonly http: HttpClient) { }
+
+  public createHubConnection(user: IUser, otherUserName: string) {
+    this.hubConnection = new HubConnectionBuilder()
+      .withUrl(this.hubUrl + "message?user=" + otherUserName, {
+        accessTokenFactory: () => user.token
+      })
+      .withAutomaticReconnect()
+      .build();
+
+    this.hubConnection.start()
+      .catch(error => console.log(error));
+
+    this.hubConnection.on("ReceiveMessageThread", messages => {
+      this.messageThreadSource.next(messages);
+    })
+
+    this.hubConnection.on('NewMessage', message => {
+      this.messageThread$.pipe(take(1)).subscribe(messages => {
+        this.messageThreadSource.next([...messages, message])
+      })
+    })
+  }
+
+  stopHubConnection() {
+    if (this.hubConnection) {
+      this.hubConnection.stop();
+    }
+  }
 
   public getMessages(container: string): Observable<IMessage[]> {
     return this.http.get<IMessage[]>(this.baseUrl, {
@@ -20,16 +55,14 @@ export class MessageService {
       }
     });
   }
-
+  
   public getMessageThread(username: string): Observable<IMessage[]> {
     return this.http.get<IMessage[]>(this.baseUrl + "thread/" + username);
   }
 
-  public sendMessage(username: string, content: string): Observable<IMessage> {
-      return this.http.post<IMessage>(this.baseUrl, {
-        "recipientName": username,
-        content
-      });
+  public async sendMessage(name: string, content: string) {
+    return this.hubConnection.invoke('SendMessage', { recipientName: name, content })
+      .catch(error => console.log(error));
   }
 
   public deleteMessage(messageId: number): Observable<any> {
