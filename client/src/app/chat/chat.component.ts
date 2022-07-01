afd9dd269f7d138c796519dd4bec57cff8fc7635
ADM3M <fly.swift499@gmail.com>
2022-07-01 16:29:02 +0300

    Updated chat component

--- client/src/app/chat/chat.component.ts
+++ client/src/app/chat/chat.component.ts
@@ -1,26 +1,43 @@
-import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnDestroy, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { take } from 'rxjs/operators';
 import { IMessage } from '../models/message';
+import { IUser } from '../models/user';
+import { AccountService } from '../services/account.service';
 import { MessageService } from '../services/message.service';
 
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
-  styleUrls: ['./chat.component.css']
+  styleUrls: ['./chat.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ChatComponent implements OnInit {
+export class ChatComponent implements OnInit, OnDestroy {
   public messages: IMessage[] = [];
   public messageContent = "";
   private username: string;
+  private user: IUser;
 
 
-  constructor(private readonly messageService: MessageService,
+  constructor(public readonly messageService: MessageService,
     private readonly route: ActivatedRoute,
-    private readonly changeDetector: ChangeDetectorRef) { }
+    private readonly changeDetector: ChangeDetectorRef,
+    private accService: AccountService) {
+      this.accService.currentUser$.pipe(take(1)).subscribe((user: IUser) => {
+        this.user = user;
+      });
+  }
+  ngOnDestroy(): void {
+    this.messageService.stopHubConnection();
+  }
 
   ngOnInit(): void {
+    this.resolveUserFromRoute();
+    this.messageService.createHubConnection(this.user, this.username);
+  }
+
+  private resolveUserFromRoute(): void {
     this.route.queryParams.pipe(take(1))
       .subscribe(params => {
         this.username = params?.username;
@@ -34,11 +51,8 @@ export class ChatComponent implements OnInit {
   }
 
   public sendMessage(form: NgForm): void {
-    this.messageService.sendMessage(this.username, this.messageContent)
-      .pipe(take(1)).subscribe((message: IMessage) => {
-        this.messages.push(message);
-        this.changeDetector.markForCheck();
-        form.reset();
-      })
+    this.messageService.sendMessage(this.username, this.messageContent).then(() => {
+      form.reset();
+    })
   }
 }
